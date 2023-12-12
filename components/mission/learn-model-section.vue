e2a6b43fb2de028606246cc87ed21966abc37507
dungdv2 <dungdv2@vmogroup.com>
2023-12-13 03:12:07 +0700

    feat: landing for fpt

---
+++ components/mission/learn-model-section.vue
@@ -0,0 +1,310 @@
+<template>
+  <div class="css-1h22ozr">
+    <div class="css-1jr2axe">
+      <div class="css-ax1dnt">
+        <h2 class="css-1ol0e8z">
+          DataCamp Recruit simplifies hiring top data talent
+        </h2>
+        <p class="css-jionp">
+          With access to 10,000+ data scientists and data analysts, you can
+          effectively find and filter professionals with the precise set of
+          skills, experience, and backgrounds that you need to hire for.
+        </p>
+      </div>
+      <div class="css-1y9ihsm">
+        <div class="css-lgbo0i">
+          <h3 class="css-1xtdx52">Hire faster</h3>
+          <p class="css-1nj7q5u">
+            It can take months to find and hire the right data scientist or data
+            analyst. With DataCamp Recruit, we streamline your candidate search
+            through smart filtering and help you get to the right hire faster.
+          </p>
+        </div>
+        <img
+          alt="candidate list"
+          height="271"
+          src="https://images.datacamp.com/image/upload/v1660202479/Talent/recruit/hire-faster.png"
+          class="css-7gdirw"
+        />
+      </div>
+      <div class="css-609a84">
+        <div class="css-lgbo0i">
+          <h3 class="css-1xtdx52">Access certified data talent</h3>
+          <p class="css-1nj7q5u">
+            Gain access to job-seekers that have been certified by DataCamp. Not
+            only have they proven their technical skills, but also their
+            abilities to communicate insights and put together compelling
+            analysis.
+          </p>
+          <div class="css-lgbo0i">
+            <p class="css-12dnjn9">
+              ‘‘As an executive I can say that I trust profesionals with the
+              DataCamp Professional Data Scientist certification.’’
+            </p>
+            <div class="css-oqdo9p">
+              <span
+                style="
+                  box-sizing: border-box;
+                  display: inline-block;
+                  overflow: hidden;
+                  width: initial;
+                  height: initial;
+                  background: none;
+                  opacity: 1;
+                  border: 0;
+                  margin: 0;
+                  padding: 0;
+                  position: relative;
+                  max-width: 100%;
+                "
+                ><span
+                  style="
+                    box-sizing: border-box;
+                    display: block;
+                    width: initial;
+                    height: initial;
+                    background: none;
+                    opacity: 1;
+                    border: 0;
+                    margin: 0;
+                    padding: 0;
+                    max-width: 100%;
+                  "
+                  ><img
+                    style="
+                      display: block;
+                      max-width: 100%;
+                      width: initial;
+                      height: initial;
+                      background: none;
+                      opacity: 1;
+                      border: 0;
+                      margin: 0;
+                      padding: 0;
+                    "
+                    alt=""
+                    aria-hidden="true"
+                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%2730%27%20height=%2730%27/%3e" /></span
+                ><img
+                  alt="Eduardo Pacheo Headshot"
+                  src="https://images.datacamp.com/image/upload/v1634642369/Marketing/Certification/JobFairPage/eduardo-pacheco.png"
+                  decoding="async"
+                  data-nimg="intrinsic"
+                  class="css-16809pz"
+                  style="
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    bottom: 0;
+                    right: 0;
+                    box-sizing: border-box;
+                    padding: 0;
+                    border: none;
+                    margin: auto;
+                    display: block;
+                    width: 0;
+                    height: 0;
+                    min-width: 100%;
+                    max-width: 100%;
+                    min-height: 100%;
+                    max-height: 100%;
+                  "
+                  srcset="
+                    https://images.datacamp.com/image/upload/v1634642369/Marketing/Certification/JobFairPage/eduardo-pacheco.png 1x,
+                    https://images.datacamp.com/image/upload/v1634642369/Marketing/Certification/JobFairPage/eduardo-pacheco.png 2x
+                  " /><noscript
+                  ><img
+                    alt="Eduardo Pacheo Headshot"
+                    srcSet="https://images.datacamp.com/image/upload/v1634642369/Marketing/Certification/JobFairPage/eduardo-pacheco.png 1x, https://images.datacamp.com/image/upload/v1634642369/Marketing/Certification/JobFairPage/eduardo-pacheco.png 2x"
+                    src="https://images.datacamp.com/image/upload/v1634642369/Marketing/Certification/JobFairPage/eduardo-pacheco.png"
+                    decoding="async"
+                    data-nimg="intrinsic"
+                    style="
+                      position: absolute;
+                      top: 0;
+                      left: 0;
+                      bottom: 0;
+                      right: 0;
+                      box-sizing: border-box;
+                      padding: 0;
+                      border: none;
+                      margin: auto;
+                      display: block;
+                      width: 0;
+                      height: 0;
+                      min-width: 100%;
+                      max-width: 100%;
+                      min-height: 100%;
+                      max-height: 100%;
+                    "
+                    class="css-16809pz"
+                    loading="lazy" /></noscript></span
+              ><span class="css-65tgt2">Eduardo Pacheo</span>
+            </div>
+          </div>
+        </div>
+        <img
+          alt="certified data talent"
+          height="276"
+          src="https://images.datacamp.com/image/upload/v1660202480/Talent/recruit/access-certified-data-talent.png"
+          class="css-1mpgwjb"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.css-1h22ozr {
+    background-color: #fffbf3;
+    padding: 96px 0;
+    position: relative;
+    z-index: 1;
+}
+.css-1jr2axe {
+    margin: auto;
+    max-width: 90%;
+    width: 1140px;
+}
+.css-ax1dnt {
+    box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 583px;
+    text-align: center;
+}
+.css-1ol0e8z {
+    font-family: Studio-Feixen-Sans,Arial,sans-serif;
+    padding: 0;
+    color: #05192D;
+    font-weight: 800;
+    font-size: 20px;
+    margin-bottom: 8px;
+    line-height: 1.25;
+    font-size: 24px;
+    margin: 0 auto;
+    text-align: center;
+}
+.css-jionp {
+    color: #05192D;
+    font-family: Studio-Feixen-Sans,Arial,sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1;
+    margin: 0;
+    padding: 0;
+    line-height: 1.5;
+    margin-bottom: 8px;
+    color: #05192D;
+    margin-top: 8px;
+    max-width: 590px;
+    text-align: center;
+}
+.css-1y9ihsm {
+    box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    margin-top: 40px;
+}
+.css-609a84 {
+    box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    margin-top: 40px;
+}
+.css-lgbo0i {
+    box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+}
+.css-1xtdx52 {
+    color: #05192D;
+    font-family: Studio-Feixen-Sans,Arial,sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+    margin: 0;
+    padding: 0;
+    color: #05192D;
+    font-weight: 800;
+    font-size: 16px;
+    margin-bottom: 8px;
+    line-height: 1.25;
+}
+.css-1nj7q5u {
+    color: #05192D;
+    font-family: Studio-Feixen-Sans,Arial,sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1;
+    margin: 0;
+    padding: 0;
+    line-height: 1.5;
+    margin-bottom: 8px;
+    color: #05192D;
+}
+.css-7gdirw {
+    height: auto;
+    margin-top: 24px;
+    width: 100%;
+}
+@media screen and (min-width: 992px) {
+  .css-1y9ihsm {
+    -webkit-flex-direction: row-reverse;
+    -ms-flex-direction: row-reverse;
+    flex-direction: row-reverse;
+    margin-top: 56px;
+}
+.css-609a84 {
+    -webkit-flex-direction: row;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    margin-top: 56px;
+}
+.css-1y9ihsm >*:first-of-type {
+    margin-right: 53px;
+}
+.css-1y9ihsm >*:first-of-type {
+    margin-right: 53px;
+}
+.css-7gdirw {
+    height: 271px;
+    margin-top: 0;
+    width: unset;
+}
+.css-609a84 >*:first-of-type {
+    margin-right: 53px;
+}
+.css-1mpgwjb {
+    height: 276px;
+    margin-left: 40px;
+    margin-right: 40px;
+    margin-top: 0;
+    width: unset;
+}
+}
+</style>
